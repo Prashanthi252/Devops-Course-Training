7658022be54e09998588292cc2e580b1db87e767
Prashanthi <gprashanthireddy.g@gmail.com>
2022-09-06 09:40:37 +0000

    commit script01.sh

---
+++ script01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Description: To print Hello world! Welcome to Shell classes using echo command
+# Author: Prashanthi <gprashanthireddy.g@gmail.com>
+# Date Created: 23-08-2022
+# Version: 
+# Date Modified:
+echo "Hello world! Welcome to Shell classes"
